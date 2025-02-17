6ea49881e8523679709ba02e7be8d35f331947d5
Pablo Almagro <pablo.almagro@ifeelonline.com>
2025-02-17 20:53:34 +0100

    delete unused imports

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from .views import UploadPDFView
-from .views import predict_view, chatbot_api, predict_costo_final_view, predict_duracion_real_view, predict_satisfaccion_cliente_view, predict_desviacion_presupuestaria_view, evaluate_feasibility, evaluate_risk
+from .views import predict_view, chatbot_api, evaluate_feasibility, evaluate_risk
 
 
 urlpatterns = [
@@ -16,10 +16,4 @@ urlpatterns = [
     path('upload-pdf/', UploadPDFView.as_view(), name='upload-pdf'),
     path('evaluate_feasibility/', evaluate_feasibility, name='evaluate_feasibility'),
     path('evaluate_risk/', evaluate_risk, name='evaluate_risk'),
-
-    #Testing Pablo
-    path("predict/costo_final/", predict_costo_final_view, name="predict_costo_final"),
-    path("predict/duracion_real/", predict_duracion_real_view, name="predict_duracion_real"),
-    path("predict/satisfaccion_cliente/", predict_satisfaccion_cliente_view, name="predict_satisfaccion_cliente"),
-    path("predict/desviacion_presupuestaria/", predict_desviacion_presupuestaria_view, name="predict_desviacion_presupuestaria"),
 ]
